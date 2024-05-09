c0eda07e5cfaea2c9f1f1649818da33a945a62e6
alizaib-coder <156937294+alizaib-coder@users.noreply.github.com>
2024-05-09 10:40:57 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,78 @@
+// const btn=document.querySelector('.button')
+// btn.addEventListener('click',function(){
+//     alert('alizaib')
+// })
+
+// const btn1=document.querySelector('')
+// btn.addEventListener('click',function(){
+//     alert('abdul waqas')
+// })
+
+
+// // Free Code and git
+// shared by talented creators
+// Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+
+// click me click me
+// facebook,linkedin,insta,watsaap
+// icon
+// Services
+// Free Stock
+
+// GitHub
+
+// Popular Searching
+
+// Collections
+
+// Challenges
+
+// Responsive Websites
+
+// Other plugins
+
+// Apps
+
+// Company
+// About
+
+// Blog
+
+// FAQ
+
+// Report Content
+
+// Become a hero
+
+// Partnerwith
+
+// Github
+
+// API
+
+// React
+
+// Popular Features
+// All Product And Feature
+
+// Popular Features
+
+// Free Meeting Scheduler App
+
+// HubSpot AI Tools
+
+// Email Tracking Software
+
+// AI Content Writer
+
+// AI Website Generator
+
+// Email Marketing Software
+
+// Lead Management Software
+
+// AI Email Writer
+
+// Customer Support
+
+// // Join a Local U
